4e191f920404c0b0646531aad61efe1a9e948981
xuzhou <xuzhoumail@gmail.com>
2014-04-28 18:05:59 +0800

    a
    a

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 	theme : 'default',
 	admin : 'admin',
 	secret : "iamxz",
-	uri : 'mongodb://root:xuzhou89@localhost:27017/cms',
+	uri : 'mongodb://root:password@localhost:27017/cms',
 };
 
 module.exports = config;
